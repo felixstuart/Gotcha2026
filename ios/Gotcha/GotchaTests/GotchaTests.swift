2cb851d99a056bbcbd7f3809cf197d865fc604e0
felixstuart <148880497+felixstuart@users.noreply.github.com>
2026-01-01 15:58:35 -0500

    initialized app

---
+++ ios/Gotcha/GotchaTests/GotchaTests.swift
@@ -0,0 +1,17 @@
+//
+//  GotchaTests.swift
+//  GotchaTests
+//
+//  Created by Felix Stuart on 1/1/26.
+//
+
+import Testing
+@testable import Gotcha
+
+struct GotchaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
